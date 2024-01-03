b37607f904884bb058e986678fd1d8c52ba35cb9
Divam Srivastava <96520711+DEATHGUN1408@users.noreply.github.com>
2024-01-03 13:49:56 +0530

    Add explanation for CrossOrigin

---
+++ Chapter-1 Inception/App.js
@@ -0,0 +1,7 @@
+// const heading=React.createElement("h1",{id:"heading",xyz:"abc"},"Hello World! From React");
+
+
+const heading=React.createElement("div",{id:"parent"},[ React.createElement("div",{id:"child1"},[React.createElement("h1",{},"i am h1"),React.createElement("h2",{},"I am h2"),]),
+React.createElement("div",{id:"child2"},[React.createElement("h1",{},"i am h1"),React.createElement("h2",{},"I am h2"),]),])
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading)
