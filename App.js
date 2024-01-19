e8edf1c7f102a4ec57d163c2a937728d2cd12935
Divam Srivastava <96520711+DEATHGUN1408@users.noreply.github.com>
2024-01-19 15:48:21 +0530

    Laying the Foundation (video-3) completed

--- App.js
+++ App.js
@@ -3,8 +3,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 const heading = React.createElement("h1", { id: "heading" }, "Namste React!");
- //JSX is not HTML in js
- const jsxheading=<h1 id="heading">Namaste React Using JSX</h1>
- consoole.log(jsxheading)
+//JSX is not HTML in js
+//JSX transppiles before itreaches js engine=>(transpiling is done by babel using parcel)
+const jsxheading = <h1 id="heading">Namaste React Using JSX</h1>;
+const Title=()=><h1 id="title">Namaste React Title</h1>;
+console.log(jsxheading);
+//Component Composition-calling one component inside one another component
+const HeadingCompponent = () => (
+  <div id="container">
+    <Title/>
+    <h1 id="heading">Namaste React Using JSX</h1>
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingCompponent />);
+
